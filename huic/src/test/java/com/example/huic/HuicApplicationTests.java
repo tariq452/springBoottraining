f928b7a8409b890e0c8327d18c2ff240d865567c
tariqtami <tariq.a.tami@gmail.com>
2023-10-23 12:23:57 +0300

    test

---
+++ huic/src/test/java/com/example/huic/HuicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.huic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
